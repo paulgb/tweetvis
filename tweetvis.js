770fbc4e05bfb4c4a71df43e1a01b71d7303dce7
Paul Butler <paulgb@gmail.com>
2014-09-28 21:39:16 -0300

    publish

--- tweetvis.js
+++ tweetvis.js
@@ -9487,6 +9487,9 @@ TweetVis = (function() {
     var container;
     d3.select('#tweetvis').remove();
     container = d3.select('body');
+    d3.select(window).on('popstate', function() {
+      return d3.select('#tweetvis').remove();
+    });
     this.div = container.append('div').attr('id', 'tweetvis').style('position', 'fixed').style('left', 0).style('top', 0).style('bottom', 0).style('right', 0).style('z-index', 1001).style('background-color', '#222');
     this.div.append('p').style('position', 'absolute').style('bottom', '20px').style('left', '20px').style('color', '#ddd').html('Visualization by <a target="_blank" href="http://paulbutler.org">Paul Butler</a>\n(<a target="_blank" href="https://twitter.com/paulgb">@paulgb</a>)\nMade with <a target="_blank" href="http://d3js.org/">d3</a>.\n<a target="_blank" href="https://github.com/paulgb/tweetvis">Source</a>');
     this.svg = this.div.append('svg:svg');
