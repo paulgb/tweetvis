3abcbaed7edc2a1a0a7915bbfdb8276c5c29db22
Paul Butler <paulgb@gmail.com>
2014-09-28 21:22:17 -0300

    charset

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://localhost:8080/tweetvis.js';})();"
+javascript:(function(){var s=document.body.appendChild(document.createElement('script'));s.setAttribute('src','https://localhost:8080/tweetvis.js');s.setAttribute('charset','utf-8')})();
